e5b93f3f0b605485386db99910a9ac4e9c13bee3
LangArthur <arthur.lang.pro@gmail.com>
2022-10-24 06:18:23 +0200

    fix basic project building

--- app/src/main/java/universim/launcher/Launcher.java
+++ app/src/main/java/universim/launcher/Launcher.java
@@ -1,4 +1,11 @@
 package universim.launcher;
 
 public class Launcher {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Launcher().getGreeting());
+    }
 }
